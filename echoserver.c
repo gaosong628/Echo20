b433f8d8bc312cf15e5905877642e686a70a8667
gaosong628 <51213408+gaosong628@users.noreply.github.com>
2022-09-12 21:03:10 -0500

    Update echoserver.c

--- echoserver.c
+++ echoserver.c
@@ -63,7 +63,7 @@ int main(int argc, char **argv) {
       exit(1);
      }
 
-      //printf("Message Received from Client: %s\n",buffer);
+      printf("Message Received from Client: %s\n",buffer);
       /*send the same message back to the client*/
       send(client_sock,buffer,strlen(buffer),0);
     }
